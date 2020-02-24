3c8aacfee52a5e90dfe71758ad8b9f3ea1befe4d
nathalia-italiano <nathalia.italiano@atmira.com>
2020-02-24 22:57:06 +0100

    investment client during a period

---
+++ InvestmentCalculator/Investment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentCalculator
+{
+    public class Investment
+    {
+        private StockHistory history;
+        private Broker broker;
+
+        public Investment(StockHistory history, Broker broker)
+        {
+            this.history = history;
+            this.broker = broker;
+        }
+        public double Open(DateTime openingDate, DateTime closeDate, double amount)
+        {
+            Wallet wallet = new Wallet();
+            DateTime currentMonth = openingDate;
+            InvestmentDateCalculator investmentDateCalculator = new InvestmentDateCalculator();
+
+            while (currentMonth < closeDate)
+            {
+                DateTime investmentDate = investmentDateCalculator.GetBuyDate(currentMonth);
+                Quote quote = history.Get(investmentDate);
+                double totalStocks = broker.BuyStocks(amount, quote);
+                wallet.AddStocks(totalStocks);
+                currentMonth = currentMonth.AddMonths(1);
+            }
+            return wallet.TotalStocks();
+        }
+        public double Close(Wallet wallet, DateTime closingDate)
+        {
+            double totalPrice = broker.SellStocks(wallet.TotalStocks(), history.Get(closingDate));
+            return totalPrice;
+        }
+    }
+}
---
+++ InvestmentCalculatorTests/InvestmentTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InvestmentCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentCalculator.Tests
+{
+    [TestClass()]
+    public class InvestmentTests
+    {
+        [TestMethod]
+        public void Test_open_should_return_total_stocks_quantity()
+        {
+            StockHistory history = new StockHistory();
+            Quote quote = new Quote(2, 4);
+            Broker broker = new Broker(50);
+            Investment investment = new Investment(history, broker);
+
+            DateTime openDate = new DateTime(2020, 2, 10);
+            DateTime closeDate = new DateTime(2020, 4, 10);
+            DateTime februaryBuyDate = new DateTime(2020, 2, 28);
+            DateTime marchBuyDate = new DateTime(2020, 3, 27);
+            double stocksQuantity = 10;
+            double amount = 20;
+            history.Add(februaryBuyDate, quote);
+            history.Add(marchBuyDate, quote);
+
+            Assert.AreEqual(stocksQuantity, investment.Open(openDate, closeDate, amount));
+
+        }
+
+        [TestMethod()]
+        public void Test_close_should_return_total_investment_amount()
+        {
+            StockHistory history = new StockHistory();
+            Quote quote = new Quote(1, 2);
+            Broker broker = new Broker(0);
+            Investment investment = new Investment(history, broker);
+
+            DateTime closeDate = new DateTime(2020, 3, 1);
+            Wallet wallet = new Wallet();
+            wallet.AddStocks(10);
+            double totalPrice = 20;
+            history.Add(closeDate, quote);
+
+            Assert.AreEqual(totalPrice, investment.Close(wallet, closeDate));
+        }
+    }
+}
