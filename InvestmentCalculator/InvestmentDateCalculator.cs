fd731ca1166839443b6c69a84ca59c38215540ba
nathalia-italiano <nathalia.italiano@atmira.com>
2020-02-24 19:32:49 +0100

    buy date for the investment

---
+++ InvestmentCalculator/InvestmentDateCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//retornar o dia da compra de aÇoes
+namespace InvestmentCalculator
+{
+    public class InvestmentDateCalculator
+    {
+        public DateTime GetBuyDate(DateTime referenceDate)
+        {
+
+            int lastDay = DateTime.DaysInMonth(referenceDate.Year, referenceDate.Month);
+            DateTime lastDate = new DateTime(referenceDate.Year, referenceDate.Month, lastDay);
+
+            while(lastDate.DayOfWeek != DayOfWeek.Thursday)
+            {
+                lastDate = lastDate.AddDays(-1);
+            }
+            return lastDate.AddDays(1);
+        }
+
+    }
+}
---
+++ InvestmentCalculatorTests/InvestmentDateCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InvestmentCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentCalculator.Tests
+{
+    [TestClass()]
+    public class InvestmentDateCalculatorTests
+    {
+        [TestMethod()]
+        public void Test_if_28_is_the_buy_date_of_february_2020()
+        {
+            InvestmentDateCalculator investmentDate = new InvestmentDateCalculator();
+
+            DateTime referenceDate = new DateTime(2020, 2, 1);
+            DateTime buyDate = new DateTime(2020, 2, 28);
+
+            Assert.AreEqual(buyDate, investmentDate.GetBuyDate(referenceDate));
+        }
+    }
+}
